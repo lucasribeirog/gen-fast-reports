b1fd711b048e4ecc34a7c70022bddbabb32a6e18
lucasribeirog <lucas-ribeiro.silva@unesp.br>
2025-01-11 19:08:34 -0300

    Inclue login pelo google

--- gen-fast-report/gen-fast-report-front/client/src/app/login/login.component.ts
+++ gen-fast-report/gen-fast-report-front/client/src/app/login/login.component.ts
@@ -6,6 +6,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import {Router} from '@angular/router';
+import { Auth, getAuth, signInWithPopup, GoogleAuthProvider,  provideAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login',
@@ -24,7 +25,7 @@ import {Router} from '@angular/router';
 export class LoginComponent {
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private router: Router) {
+  constructor(private fb: FormBuilder, private router: Router, private auth: Auth) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
@@ -38,5 +39,17 @@ export class LoginComponent {
     } else {
       console.log('Form is invalid');
     }
+  }
+  googleLogin() {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(this.auth, provider)
+      .then((result) => {
+        console.log('Logged in with Google:', result.user);
+        // Redirect to home page
+        this.router.navigate(['/home']);
+      })
+      .catch((error) => {
+        console.log('Error during Google login:', error);
+      });
   }
 }
